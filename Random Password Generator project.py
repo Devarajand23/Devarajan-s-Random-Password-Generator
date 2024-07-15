35b25ce0bbedfa12e20634d29fd236b40300a71c
Devarajand23 <ddevarajan109@gmail.com>
2024-07-15 20:14:36 +0530

    Add files via upload

---
+++ Random Password Generator project.py
@@ -0,0 +1,19 @@
+import random
+
+print("Welcome to Devarajan's Random Password Generator")
+
+randomchars = ("ABCDEFGHJIJKLMNOPQSTUVWXYZabcdefghjiklmnopqrstuvwxyz@$&*1234567890")
+
+numberofpassword = int(input("Please Enter the Numnber of Password to be Generated :"))
+
+passwordlength = int(input("Please Enter the Length of the Password to be Generated :"))
+
+print("Here are the passwords :")
+
+for x in range(numberofpassword):
+    password = ""
+    for chars in range(passwordlength):
+        password = password + random.choice(randomchars)
+    print(password)
+
+print("Thank for using Devarajan's Random Password Generator !")
